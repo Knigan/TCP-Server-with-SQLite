db40385278aaaa5d269f641a8a2caaeba16839c9
Khapisov Malik <81933036+Knigan@users.noreply.github.com>
2022-08-24 19:53:28 +0300

    Update client.py

--- client.py
+++ client.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     is_started = False
     while IS_RECONNECT_ENABLED or not is_started:
         is_started = True
-        print()
         print("Create client")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((HOST, PORT))
